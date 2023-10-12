5f61557d24f157041dc2c4b8d66a9aaaef69e0cd
Luan Carminatti <luancarminatt@gmail.com>
2023-10-11 21:04:58 -0300

    feature media for image

--- project_setup/app_luancarminatti/models.py
+++ project_setup/app_luancarminatti/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=255)
-    image = models.ImageField(upload_to='projects/images/')  # Alterado o caminho para 'projects/images/'
+    image = models.ImageField(upload_to='media/')  
     date_at = models.DateTimeField(auto_created=True, auto_now=True)
 
     def __str__(self):

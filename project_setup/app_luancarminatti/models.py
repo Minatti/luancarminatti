3aaafd223d77ae15db6db1acef064bf9dfaa32b3
Luan Carminatti <luancarminatt@gmail.com>
2023-10-11 21:10:55 -0300

    remove dir media in media, other files

--- project_setup/app_luancarminatti/models.py
+++ project_setup/app_luancarminatti/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=255)
-    image = models.ImageField(upload_to='media/')  
+    image = models.ImageField(upload_to='projects/images/')  
     date_at = models.DateTimeField(auto_created=True, auto_now=True)
 
     def __str__(self):
